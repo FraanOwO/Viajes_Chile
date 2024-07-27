bcaf95b0e1708807edd3db56c98971a95181805d
FraanOwO <168241701+FraanOwO@users.noreply.github.com>
2024-07-27 15:11:19 -0400

    cambios Css

---
+++ assets/js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    // Inicializa los tooltips de Bootstrap
+    $('[data-toggle="tooltip"]').tooltip();
+
+    // Opciones del carrusel
+    $('.carousel').carousel({
+        interval: 1
+    });
+});
+
+
+
+
